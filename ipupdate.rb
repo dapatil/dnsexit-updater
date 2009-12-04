3756d25ea784e85c878bdad22b61df73edee7b88
Darshan Patil <dapatil@gmail.com>
2009-12-04 01:32:12 -0500

    Only update DNS record when IP changes

--- ipupdate.rb
+++ ipupdate.rb
@@ -11,6 +11,7 @@
 # */10 *  *   *   *    /path/to/ipupdate.rb
 #
 require 'net/http'
+require 'socket'
 require 'cgi'
 
 # Configuration
@@ -18,10 +19,34 @@ USERNAME='username'
 PASSWORD='secret'
 HOST='owomi.com'
 
-host = Net::HTTP.new("www.dnsexit.com", 80)
-url = "/RemoteUpdate.sv?login=#{CGI.escape(USERNAME)}" +
-      "&password=#{CGI.escape(PASSWORD)}" +
-      "&host=#{CGI.escape(HOST)}"
+# Hits checkip.dyndns.com and finds out your external IP
+def myIP
+  svc = Net::HTTP.new("checkip.dyndns.com", 80)
+  _, data = svc.get("/", nil)
+  if data
+    return data.match(/\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/).to_s
+  else
+    return nil
+  end
+end
 
-resp, data = host.get(url, nil)
-puts data
+# Returns current IP for HOST
+def domainIP
+  return Socket.getaddrinfo(HOST, 'http')[0][3]
+end
+
+# Update dnsexit
+def updateDNS
+  puts "IP change detected - Updating DNS"
+  host = Net::HTTP.new("www.dnsexit.com", 80)
+  url = "/RemoteUpdate.sv?login=#{CGI.escape(USERNAME)}" +
+    "&password=#{CGI.escape(PASSWORD)}" +
+    "&host=#{CGI.escape(HOST)}"
+
+  resp, data = host.get(url, nil)
+  puts data
+end
+
+if __FILE__ == $0
+  updateDNS if myIP != domainIP
+end
